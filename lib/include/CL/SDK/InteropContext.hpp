eeb1c7c090b87ccdd0a3429816a89589462b14f5
Máté Ferenc Nagy-Egri <mate@streamhpc.com>
2021-11-30 15:08:34 +0100

    Match definition to declaration

--- lib/include/CL/SDK/InteropContext.hpp
+++ lib/include/CL/SDK/InteropContext.hpp
@@ -10,7 +10,7 @@ namespace cl
 {
 namespace sdk
 {
-    vector<cl_context_properties> SDKCPP_EXPORT get_interop_context_properties(const cl::Device& plat);
+    vector<cl_context_properties> SDKCPP_EXPORT get_interop_context_properties(const cl::Device& plat, cl_int* error = nullptr);
 
     Context SDKCPP_EXPORT get_interop_context(int plat_id, int dev_id, cl_device_type type, cl_int* error = nullptr);
 }

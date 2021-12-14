8f677a2a649d2d275cccc8b76a096a4b4749f0cd
Máté Ferenc Nagy-Egri <mate@streamhpc.com>
2021-12-14 14:01:50 +0100

    Apply git-format

--- lib/include/CL/Utils/InteropContext.hpp
+++ lib/include/CL/Utils/InteropContext.hpp
@@ -5,12 +5,14 @@
 
 #include <CL/opencl.hpp>
 
-namespace cl
-{
-namespace util
-{
-    vector<cl_context_properties> UTILSCPP_EXPORT get_interop_context_properties(const cl::Device& plat, cl_int* error = nullptr);
+namespace cl {
+namespace util {
+    vector<cl_context_properties>
+        UTILSCPP_EXPORT get_interop_context_properties(const cl::Device& plat,
+                                                       cl_int* error = nullptr);
 
-    Context UTILSCPP_EXPORT get_interop_context(int plat_id, int dev_id, cl_device_type type, cl_int* error = nullptr);
+    Context UTILSCPP_EXPORT get_interop_context(int plat_id, int dev_id,
+                                                cl_device_type type,
+                                                cl_int* error = nullptr);
 }
 }
--- lib/src/SDK/InteropWindow.cpp
+++ lib/src/SDK/InteropWindow.cpp
@@ -2,21 +2,14 @@
 #include <CL/SDK/InteropWindow.hpp>
 #include <CL/SDK/InteropContext.hpp>
 
-cl::sdk::InteropWindow::InteropWindow(
-    sf::VideoMode mode,
-    const sf::String& title,
-    sf::Uint32 style,
-    const sf::ContextSettings& settings,
-    cl_uint platform_id,
-    cl_uint device_id,
-    cl_bitfield device_type
-)
-    : sf::Window{mode, title, style, settings}
-    , plat_id{platform_id}
-    , dev_id{device_id}
-    , dev_type{device_type}
-{
-}
+cl::sdk::InteropWindow::InteropWindow(sf::VideoMode mode,
+                                      const sf::String& title, sf::Uint32 style,
+                                      const sf::ContextSettings& settings,
+                                      cl_uint platform_id, cl_uint device_id,
+                                      cl_bitfield device_type)
+    : sf::Window{ mode, title, style, settings }, plat_id{ platform_id },
+      dev_id{ device_id }, dev_type{ device_type }
+{}
 
 void cl::sdk::InteropWindow::run()
 {
@@ -26,12 +19,15 @@ void cl::sdk::InteropWindow::run()
 
     opencl_context = get_interop_context(plat_id, dev_id, dev_type);
 
-    cl_khr_gl_event_supported =
-        opencl_context.getInfo<CL_CONTEXT_DEVICES>().at(0).getInfo<CL_DEVICE_EXTENSIONS>().find("cl_khr_gl_event") != cl::string::npos;
+    cl_khr_gl_event_supported = opencl_context.getInfo<CL_CONTEXT_DEVICES>()
+                                    .at(0)
+                                    .getInfo<CL_DEVICE_EXTENSIONS>()
+                                    .find("cl_khr_gl_event")
+        != cl::string::npos;
 
     initializeCL();
 
-    while(isOpen())
+    while (isOpen())
     {
         render();
 
@@ -40,8 +36,7 @@ void cl::sdk::InteropWindow::run()
         updateScene();
 
         sf::Event ev;
-        while (pollEvent(ev))
-            event(ev);
+        while (pollEvent(ev)) event(ev);
     }
 
     setActive(false);

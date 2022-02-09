8b28e4089f2c987b8cfdd319858df849c8c31c46
Máté Ferenc Nagy-Egri <mate@streamhpc.com>
2022-02-09 17:24:11 +0100

    OpenGL to Dependencies.cmake

--- cmake/Dependencies.cmake
+++ cmake/Dependencies.cmake
@@ -5,22 +5,17 @@ if(OPENCL_SDK_BUILD_SAMPLES)
   endforeach()
 
   if(OPENCL_SDK_BUILD_OPENGL_SAMPLES)
-    foreach(DEP IN ITEMS glm)
+    foreach(DEP IN ITEMS glm OpenGL)
       list(APPEND CMAKE_MODULE_PATH "${CMAKE_CURRENT_LIST_DIR}/Dependencies/${DEP}")
       include(${DEP})
     endforeach()
 
     cmake_minimum_required(VERSION 3.10) # SFML 2 won't find Freetype::Freetype under 3.10
-    find_package(OpenGL REQUIRED)
     if(CMAKE_SYSTEM_NAME MATCHES Linux) # TODO: Add EGL support
       # OpenGL doesn't explicitly depend on X11 (as of CMake v3.2) but we'll need it
       find_package(X11 REQUIRED)
     endif()
     find_package(GLEW REQUIRED)
-    if(NOT TARGET OpenGL::GLU)
-      # GLU is a dependency of GLEW but it's not advertized as an OpenGL COMPONENT
-      message(FATAL_ERROR "GLEW depends on GLU but was not found.")
-    endif()
     find_package(SFML 2
       REQUIRED
       COMPONENTS window graphics
---
+++ cmake/Dependencies/OpenGL/OpenGL.cmake
@@ -0,0 +1,18 @@
+if(NOT DEPENDENCIES_FORCE_DOWNLOAD)
+  find_package(OpenGL)
+endif()
+
+if(NOT OpenGL_FOUND)
+  if(WIN32)
+    message(FATAL_ERROR "OpenGL development files not found. Please install some version of the Windows SDK.")
+  elseif(Linux)
+    message(FATAL_ERROR "OpenGL development files not found. Please install them using your system package manager, for eg. libgl-dev")
+  else(Apple)
+    message(FATAL_ERROR "OpenGL development files not found.")
+  endif()
+endif()
+
+if(NOT TARGET OpenGL::GLU)
+  # GLU is a dependency of GLEW but it's not advertized as an OpenGL COMPONENT
+  message(FATAL_ERROR "GLU is a dependency of GLEW but was not found.")
+endif()

a92f63d113936a8aa0a4b671802eaa65612a990b
Máté Ferenc Nagy-Egri <mate@streamhpc.com>
2022-02-11 11:22:10 +0100

    All deps to Dependencies.cmake

--- cmake/Dependencies.cmake
+++ cmake/Dependencies.cmake
@@ -5,16 +5,12 @@ if(OPENCL_SDK_BUILD_SAMPLES)
   endforeach()
 
   if(OPENCL_SDK_BUILD_OPENGL_SAMPLES)
-    foreach(DEP IN ITEMS X11 glm OpenGL)
+    foreach(DEP IN ITEMS X11 glm OpenGL GLEW SFML)
       list(APPEND CMAKE_MODULE_PATH "${CMAKE_CURRENT_LIST_DIR}/Dependencies/${DEP}")
       include(${DEP})
     endforeach()
-
-    cmake_minimum_required(VERSION 3.10) # SFML 2 won't find Freetype::Freetype under 3.10
-    find_package(GLEW REQUIRED)
-    find_package(SFML 2
-      REQUIRED
-      COMPONENTS window graphics
-    )
+    if(TARGET libglew_static)
+      message(STATUS libglew_static exists)
+    endif()
   endif(OPENCL_SDK_BUILD_OPENGL_SAMPLES)
 endif(OPENCL_SDK_BUILD_SAMPLES)
--- cmake/Dependencies/TCLAP/TCLAP.cmake
+++ cmake/Dependencies/TCLAP/TCLAP.cmake
@@ -12,16 +12,12 @@ if(NOT TCLAP_FOUND)
   endif()
   cmake_minimum_required(VERSION 3.11)
   include(FetchContent)
+  set(TCLAP_BUILD_EXAMPLES OFF CACHE BOOL "Build TCLAP examples")
   FetchContent_Declare(
     tclap-external
     GIT_REPOSITORY      https://github.com/mirror/tclap.git
     GIT_TAG             v1.2.5 # 58c5c8ef24111072fc21fb723f8ab45d23395809
-    UPDATE_COMMAND      ""
-    PATCH_COMMAND       ""
-    CONFIGURE_COMMAND   ""
-    BUILD_COMMAND       ""
-    INSTALL_COMMAND     ""
-    TEST_COMMAND        ""
+    PATCH_COMMAND       ${CMAKE_COMMAND} -E copy "${CMAKE_CURRENT_LIST_DIR}/CMakeLists.txt" "${CMAKE_CURRENT_BINARY_DIR}/_deps/tclap-external-src/CMakeLists.txt"
   )
   FetchContent_MakeAvailable(tclap-external)
   list(APPEND CMAKE_PREFIX_PATH "${CMAKE_CURRENT_BINARY_DIR}/_deps/tclap-external-src/include")

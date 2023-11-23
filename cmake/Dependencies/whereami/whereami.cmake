8b2bb152bbe6188353233f0390991ba817dda652
Máté Ferenc Nagy-Egri <mate@streamhpc.com>
2023-11-23 14:39:59 +0100

    Handle whereami with variables

--- cmake/Dependencies/whereami/whereami.cmake
+++ cmake/Dependencies/whereami/whereami.cmake
@@ -6,10 +6,18 @@ FetchContent_Declare(
   GIT_TAG        ba364cd54fd431c76c045393b6522b4bff547f50 # master @ 2023.04.20.
 )
 FetchContent_MakeAvailable(whereami-external)
-add_library(whereami IMPORTED INTERFACE)
-target_include_directories(whereami
-  INTERFACE "${CMAKE_CURRENT_BINARY_DIR}/_deps/whereami-external-src/src"
-)
-target_sources(whereami
-  INTERFACE "${CMAKE_CURRENT_BINARY_DIR}/_deps/whereami-external-src/src/whereami.c"
-)
+# NOTE: This works, though not elegant
+set(whereami_SOURCES "${CMAKE_CURRENT_BINARY_DIR}/_deps/whereami-external-src/src/whereami.c")
+set(whereami_INCLUDE_DIR "${CMAKE_CURRENT_BINARY_DIR}/_deps/whereami-external-src/src")
+# NOTE: this would be ideal, however for some reason on the exported OpenCL::Utils
+#       lib an INTERFACE_LINK_LIBRARIES $<LINK_ONLY:whereami> prop appears which
+#       is nonsense, LINK_ONLY makes no sense for a dep that OpenCL::Utils linked
+#       to PRIVATEly.
+#
+#add_library(whereami IMPORTED INTERFACE)
+#target_include_directories(whereami
+#  INTERFACE "${CMAKE_CURRENT_BINARY_DIR}/_deps/whereami-external-src/src"
+#)
+#target_sources(whereami
+#  INTERFACE "${CMAKE_CURRENT_BINARY_DIR}/_deps/whereami-external-src/src/whereami.c"
+#)
